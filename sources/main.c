2154c8090333ec023030fc99f3adf72e5dedb355
Snorky <snorky2001@gmail.com>
2014-07-05 23:55:01 +0200

    minimal CLI

--- sources/main.c
+++ sources/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+void DisplayWelcomeMessage(void)
+{
+    printf("TaskReminder v0.1\r\n");
+}
+
 int main(int argc, char *argv[])
 {
-	printf("Hello!\n");
+    char command[100];
+    DisplayWelcomeMessage();
+	
+    do
+    {
+        printf(">>");
+        fgets (command , 100, stdin);
+    } while (command[0]!='q');
 	return 0;
 }
