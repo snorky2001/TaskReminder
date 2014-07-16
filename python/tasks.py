0355543fb419b1666bd510c21462f551e30626d4
Snorky2001 <snorky2001@hotmail.com>
2014-07-16 16:22:12 +0200

    Add comments to lib

--- python/tasks.py
+++ python/tasks.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python 
+#
+# Library to manage a list of task repeated at fixed interva Library to manage a list of task repeated at fixed intervall
+#
+
 import sys
 import pickle
 from datetime import timedelta
@@ -5,29 +10,39 @@ from datetime import datetime
 
 
 def CreateEmptyTaskList( ):
+	# Create a new empty taskList
 	# taskList = list (firstAvailableId, tasks dict)
 	return [0, {}]
 
-def AddTask( taskList, Name, Description, Interval, Reminder ):
+def AddTask(taskList, Name, Description, Interval, Reminder):
+	# Add a task to a taskList
 	firstAvailableId = taskList[0]
 	tasks = taskList[1]
 	tasks[firstAvailableId] = (Name, Description, Interval, Reminder, datetime.max)
 	taskList[0] = firstAvailableId + 1
 
-def SaveTasks( taskList, filename ):
+def SaveTasks(taskList, filename):
+	# Save the taskList in a file
+	# Can return IOError exception for wrong filename
 	version = 1.0
 	with open(filename, 'wb') as output:
 		pickle.dump(version, output, pickle.HIGHEST_PROTOCOL)
 		pickle.dump(taskList[0], output, pickle.HIGHEST_PROTOCOL)
 		pickle.dump(taskList[1], output, pickle.HIGHEST_PROTOCOL)
 
-def LoadTasks( taskList, filename ):
+def LoadTasks(taskList, filename):
+	# Load a taskList from a file
+	# Can return IOError exception for wrong filename
 	with open(filename, 'rb') as input:
 		version = pickle.load(input)
 		taskList[0] = pickle.load(input)
 		taskList[1] = pickle.load( input)
 
-def CheckTasks( taskList, checkDate ):
+def CheckTasks(taskList, checkDate):
+	# Check the taskList status at checkDate date
+	# return a tuple (dueIn, lateOf)
+	# where dueIn is a list of (Id, delta to due date)
+	# and lateOf is a list of (Id, delta passed of due date)
 	dueIn = []
 	lateOf = []
 	for k,v in taskList[1].iteritems():
@@ -41,40 +56,50 @@ def CheckTasks( taskList, checkDate ):
 					dueIn.append( (k, checkDate - dueDate) )
 	return (dueIn, lateOf)
 
-def CheckTaskId( taskList, taskId):
+def CheckTaskId(taskList, taskId):
+	# Check if the taskId is a valid Id in taskList
 	if taskId in taskList[1].keys():
 		return True
 	else:
 		return False
 
-def DeleteTask( taskList, taskId):
+def DeleteTask(taskList, taskId):
+	# Delete the task with taskId from the taskList
+	# Can return IndexError if invalid taskId is passed
 	if taskId in taskList[1].keys():
 		del taskList[1][taskId]
 	else:
 		raise IndexError
 
-def UpdateTask( taskList, taskId, Name, Description, Interval, Reminder, ValidatedDate ):
+def UpdateTask(taskList, taskId, Name, Description, Interval, Reminder, ValidatedDate):
+	# Update the task with taskId from the taskList
+	# Can return IndexError if invalid taskId is passed
 	if taskId in taskList[1].keys():
 		taskList[1][taskId] = (Name, Description, Interval, Reminder, ValidatedDate)
 	else:
 		raise IndexError
 
-def GetTask( taskList, taskId):
+def GetTask(taskList, taskId):
+	# Return the task with taskId from the taskList
+	# Can return IndexError if invalid taskId is passed
 	if taskId in taskList[1].keys():
 		return taskList[1][taskId]
 	else:
 		raise IndexError
 
-def GetTasks( taskList):
+def GetTasks(taskList):
+	# Return the list of all tasks in taskList
 	return taskList[1]
 
-def ValidateTask( taskList, taskId, ValidateDate ):
+def ValidateTask(taskList, taskId, ValidateDate):
+	# Set the validation time of the task with id taskId
+	# The ValidateDate is ued to compute the next DueDate
 	if taskId in taskList[1].keys():
 		taskList[1][taskId] = (taskList[1][taskId][0],
-							   taskList[1][taskId][1],
-							   taskList[1][taskId][2],
-							   taskList[1][taskId][3],
-							   ValidateDate)
+				   taskList[1][taskId][1],
+				   taskList[1][taskId][2],
+				   taskList[1][taskId][3],
+				   ValidateDate)
 	else:
 		raise IndexError
 
